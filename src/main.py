4becff5f20e34002010bd2469b55a92c94c06d26
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-08-25 11:52:15 -0300

    testando e criando classe service

---
+++ src/main.py
@@ -0,0 +1,2 @@
+from models.service import Service
+serv = Service("MyService")
---
+++ src/models/service.py
@@ -0,0 +1,5 @@
+class Service:
+    def __init__(self, name: str):
+        self.name = name
+    def start(self):
+        pass
