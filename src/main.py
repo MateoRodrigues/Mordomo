286c65d122a8c077832c22f56f02f73cb35bdb82
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-09-02 18:04:00 -0300

    criando arquivos csv e classe para manipularlos

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
 from models.service import Service
+from models.filecsv import FileCSV
 serv = Service("MyService")
-print(serv.name)
+filcsv = FileCSV("/home/matheus/Documentos/python/visby/data/tasks.csv")
+filcsv.write_csv([[f"{serv.name}"], [f"{serv.description}"]])
---
+++ src/models/filecsv.py
@@ -0,0 +1,16 @@
+import csv
+
+class FileCSV:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def read_csv(self):
+        with open(self.filepath, mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            data = [row for row in reader]
+        return data
+
+    def write_csv(self, data):
+        with open(self.filepath, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
