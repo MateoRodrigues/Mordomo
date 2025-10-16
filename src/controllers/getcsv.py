b1b3e6bfabdb60ef50d777422ed905544013a181
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-10-16 16:39:25 -0300

    Adicionando o método get_tdata para adicionar os dados automáticamente nas colunar do treeview.

--- src/controllers/getcsv.py
+++ src/controllers/getcsv.py
@@ -4,11 +4,10 @@ class GetCSV:
     def __init__(self):
         self.filcsv = FileCSV()
         self.tchaves = tuple()
-        self.data = []
+        self.data = self.filcsv.read_csv()
 
     def get_data(self):
         # data é uma lista de dicionários
-        self.data = self.filcsv.read_csv()
         return self.data
     def get_chaves(self):
         chaves = self.filcsv.get_chaves()
@@ -16,6 +15,19 @@ class GetCSV:
             # nome das colunas do csv em uma tupla
             self.tchaves += (c,)
         return self.tchaves
+    def get_tdata(self):
+        # tdata é uma lista de tuplas
+        self.tdata = list()
+        self.titem = tuple()
+        for pos, service in enumerate(self.data):
+            for value in service.values():
+                last_item = list(service.values())[-1]
+                self.titem += (value,)
+                if (value) == last_item:
+                    self.tdata.append(self.titem)
+                    self.titem = tuple()
+        return self.tdata
+        
 
 
         
