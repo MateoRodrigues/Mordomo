93be07d4d67e0b94cebb56b79421d64329ebfdcb
MateoRodrigues <matheus.machado.curso@gmail.com>
2026-01-28 16:48:43 -0300

    adiciona a leitura do arquivo data/service.csv na forma de tabela no terminal.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
-from view.read_csv_view import ReadCSVView
-from controllers.getcsv import GetCSV
+from rich.table import Table
+from rich import print
+from repository.filecsv import FileCSV
 
-rd = ReadCSVView()
-rd.mainloop()
+chaves = FileCSV().get_chaves()
+cores = ["cyan", "magenta", "green", "yellow", "blue"]
+data = FileCSV().read_csv()
+tabela = Table(title="Serviços Cadastrados")
+for chave, cor in zip(chaves, cores):
+    tabela.add_column(chave, justify="center", style=cor)
+for d in data:
+    tabela.add_row(d['nome'], d['descricao'], d['data_do_serviço'], d['cliente'], d['valor'])
+print(tabela)
+
+
+
