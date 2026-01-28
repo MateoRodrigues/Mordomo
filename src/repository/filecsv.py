8934095bad3b9d166b08ac4aec2a4892b664a386
MateoRodrigues <matheus.machado.curso@gmail.com>
2026-01-28 16:47:09 -0300

    adiciona uma docstring a classe FileCSV, modificação no arquivo src/repository/filecsv.py.

--- src/repository/filecsv.py
+++ src/repository/filecsv.py
@@ -2,6 +2,11 @@ import csv
 from pathlib import Path
 
 class FileCSV:
+    """ Esta classe é responsável por ler e escrever arquivos CSV.
+     EXEMPLO DE USO:
+        file_csv = FileCSV()
+        data, reader = file_csv.read_csv()
+        file_csv.write_csv(['novo', 'registro', 'csv'])"""
    
     def __init__(self, filepath=Path(__file__).parent.parent.parent/'data'/'service.csv'):
         self.filepath = filepath
@@ -9,10 +14,9 @@ class FileCSV:
     def read_csv(self):
         with open(self.filepath, mode='r', newline='', encoding='utf-8') as file:
             reader = csv.DictReader(file)
+            #values = [row for row in csv.reader(file)]
             data = [row for row in reader]
         return data
-
-
 
     def write_csv(self, data):
         with open(self.filepath, mode='a', newline='', encoding='utf-8') as file:
